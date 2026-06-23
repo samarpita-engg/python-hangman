aa43e35f12ecd443aa6d3b2b44fc122bcba27dc3
samarpita-engg <samarpitabanerjee07@gmail.com>
2026-06-24 00:25:33 +1000

    Step 1

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("Welcome to Hangman!")
+
+import random
+
+word_list = ["aardvark", "baboon", "camel", "clock", "music", "weekend"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+guess = input("Guess a letter that might be in the word:\n").lower()
+
+for letter in chosen_word:
+    if guess == letter:
+        print("Right")
+    else:
+        print("Wrong")
